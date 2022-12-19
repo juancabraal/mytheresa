6d4fb8e2138aeb6859f5c1b2fa54af5dfa4fdad3
Juan Cabral <juancabraal@gmail.com>
2022-12-19 23:04:28 +0100

    add dotenv to handle environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const dotenv = require("dotenv").config({
+  path: path.join(__dirname, ".env"),
+});
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
@@ -9,6 +14,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.parsed),
     }),
   ],
   devServer: {

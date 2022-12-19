6d9ee43508f851a90f49fe30af90bbc60ad15df0
Juan Cabral <juancabraal@gmail.com>
2022-12-19 22:26:21 +0100

    set base url with environment variable

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,10 @@
 import Axios from "axios";
+import envs from "../utils/envs";
 import { applyAuthInterceptor } from "./authInterceptor";
 
 export const axios = Axios.create({
   responseType: "json",
+  baseURL: envs.API_URL,
 });
 
 applyAuthInterceptor(axios);

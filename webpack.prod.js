f226056509530e810353e23c8f5a5642d8310f3e
Juan Cabral <juancabraal@gmail.com>
2022-12-20 13:15:02 +0100

    fix node_env on prod environment

--- webpack.prod.js
+++ webpack.prod.js
@@ -20,6 +20,9 @@ module.exports = merge(config, {
       minRatio: 0.8,
       deleteOriginalAssets: false,
     }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
   ],
   optimization: {
     minimize: true,

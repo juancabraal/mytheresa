afce7d7818cd49c5df42ef0dd40dfc024adb0d23
Juan Cabral <juancabraal@gmail.com>
2022-12-21 16:51:11 +0100

    add wish page

--- src/pages/wish/index.js
+++ src/pages/wish/index.js
@@ -1,5 +1,35 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MovieItem from "../../components/movieItem";
+import { WishListContext } from "../../context/WishListContext";
+
+import "./style.scss";
+
 const WishPage = () => {
-  return <p>WishPage</p>;
+  const { wishList, removeItemFromList } = useContext(WishListContext);
+  const navigate = useNavigate();
+  const APP_URL = process.env.APP_URL;
+
+  return (
+    <div className="wish-list-container">
+      <div className="title">
+        <h2>Wish List</h2>
+      </div>
+      <div className="list-container">
+        {wishList.map((value) => (
+          <MovieItem
+            key={value.id}
+            movie={value}
+            onClick={() =>
+              navigate(`${APP_URL}detail/${value.category}/${value.id}`)
+            }
+            onRemove={() => removeItemFromList(value.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default WishPage;

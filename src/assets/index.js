4c24e959e6de12743e28674770458f94429e5dc7
Juan Cabral <juancabraal@gmail.com>
2022-12-21 22:02:39 +0100

    replace cart image for heart icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,1 @@
 export { default as MovieLogo } from "./images/movie.svg";
-export { default as CartImage } from "./images/cart.svg";
--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { CartImage } from "../../assets";
+import { FaHeart } from "react-icons/fa";
+
 import { WishListContext } from "../../context/WishListContext";
 import Button from "../button";
 import "./style.scss";
@@ -16,7 +17,7 @@ const Cart = () => {
         className="cart-button-action"
         onClick={() => navigate(`${APP_URL}wish-list`)}
       >
-        <img src={CartImage} />
+        <FaHeart fontSize={25} />
         {wishList.length > 0 && (
           <div className="cart-badge">{wishList.length}</div>
         )}

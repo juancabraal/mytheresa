42f62734572f5f0698d3e6959faaee0de170db4b
Juan Cabral <juancabraal@gmail.com>
2022-12-21 16:51:24 +0100

    add home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,39 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Carousel from "../../components/carousel";
+import CATEGORIES from "../../consts/categories";
+import "./style.scss";
 const HomePage = () => {
-  return <p>HomePage</p>;
+  const { search: urlSearch } = useLocation();
+  useEffect(() => {
+    const slideSection = urlSearch.slice(1).toUpperCase();
+    if (
+      urlSearch &&
+      urlSearch.startsWith("?") &&
+      Object.keys(CATEGORIES).indexOf(slideSection) >= 0
+    ) {
+      const slideElement = document.getElementById(slideSection);
+
+      if (slideElement) {
+        slideElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [urlSearch]);
+  return (
+    <div className="home-container">
+      <div className="movie-section" id={CATEGORIES.POPULAR}>
+        <Carousel category={CATEGORIES.POPULAR} />
+      </div>
+      <div className="movie-section" id={CATEGORIES.TOP_RATED}>
+        <Carousel category={CATEGORIES.TOP_RATED} />
+      </div>
+      <div className="movie-section" id={CATEGORIES.UPCOMING}>
+        <Carousel category={CATEGORIES.UPCOMING} />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;

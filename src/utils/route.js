640269727069642a750fa0c72649617811deb632
Juan Cabral <juancabraal@gmail.com>
2022-12-22 23:15:51 +0100

    fix double slash on router

--- src/utils/route.js
+++ src/utils/route.js
@@ -2,7 +2,5 @@ export const getApplicationRoute = (path = "") => {
   const APP_URL = process.env.APP_URL;
   const urlPath = [APP_URL, path].join("");
 
-  if (urlPath.slice(0, 2) === "//") return urlPath.slice(1);
-
-  return urlPath;
+  return urlPath.replace("//", "/");
 };

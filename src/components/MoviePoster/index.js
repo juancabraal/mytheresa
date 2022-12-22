3c91d8b42e582ef4affed179a701a999c646f062
Juan Cabral <juancabraal@gmail.com>
2022-12-22 21:49:22 +0100

    delete unused components

--- src/components/MoviePoster/index.js
+++
@@ -1,9 +0,0 @@
-const MoviePoster = ({ posterUrl, fadeProps }) => {
-  return (
-    <div className="movie-post-image">
-      <img src={posterUrl && `http://image.tmdb.org/t/p/w500/${posterUrl}`} />
-    </div>
-  );
-};
-
-export default MoviePoster;

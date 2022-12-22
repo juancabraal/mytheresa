08860175d8cf0d667aacffedcec7137b0d46be3c
Juan Cabral <juancabraal@gmail.com>
2022-12-22 23:21:54 +0100

    fix find language helper function

--- src/utils/hooks/useLanguages.js
+++ src/utils/hooks/useLanguages.js
@@ -3,7 +3,10 @@ export const useLanguages = () => {
   const languages = LANGUAGES;
 
   const findLanguage = (language) => {
-    return languages.find((value) => value.key === language) || languages[0];
+    return (
+      languages.find((value) => String(value.key).toLowerCase() === language) ||
+      languages[0]
+    );
   };
 
   return { languages, findLanguage };

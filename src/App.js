3eb64d4df20fa2bab823147ec475f7c5ea031fe5
Juan Cabral <juancabraal@gmail.com>
2022-12-22 12:26:02 +0100

    fix absolute height on mobile

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import { WishListProvider } from "./context/WishListContext";
 
 import MainRouter from "./router";
+import { normalizeWindowHeight } from "./utils/heightDimension";
 import { composeWrappers } from "./utils/reactUtils";
 
 const App = () => {
   const Providers = useMemo(() => {
     return composeWrappers([BrowserRouter, {}], [WishListProvider, {}]);
   });
+  normalizeWindowHeight();
   return (
     <Providers>
       <MainRouter />

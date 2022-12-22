aca1c02ee032bb1f58a178a043b6d5dd9e2e7f19
Juan Cabral <juancabraal@gmail.com>
2022-12-22 22:00:54 +0100

    refactor fade up animation

--- src/components/animations/FadeUp/index.js
+++ src/components/animations/FadeUp/index.js
@@ -1,11 +1,19 @@
 import { motion } from "framer-motion";
 
-const FadeUp = ({ children, ...rest }) => {
+const variants = {
+  visible: { y: 0, opacity: 1 },
+  hidden: { y: 30, opacity: 0 },
+};
+
+const FadeUp = ({ children, animationKey, ...rest }) => {
   return (
     <motion.div
-      initial={{ y: 30, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
+      key={animationKey}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
       transition={{ duration: 0.5 }}
+      variants={variants}
       {...rest}
     >
       {children}
